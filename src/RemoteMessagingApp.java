2aa9e80b19c513fdd1c961123c52fe12fc9fbc22
Αργυρώ Μπανιώτη <abani@csd.auth.gr>
2025-11-30 17:19:18 +0200

    updated functions in RemoteMessagingApp

--- src/RemoteMessagingApp.java
+++ src/RemoteMessagingApp.java
@@ -3,122 +3,9 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class RemoteMessagingApp extends UnicastRemoteObject implements MessagingAppInt
 {
+
     public RemoteMessagingApp() throws RemoteException {
         super();
-    }
-
-
-
-    public String CreateAccount(String username) throws RemoteException
-    {
-        String output;
-        if(!IsUsernameValid(username))
-        {
-            output="Invalid Username";
-        }
-        else
-        {
-            boolean accountExists = false;
-            for (Account account : MessagingServer.accounts) {
-                if (account.username.equals(username)) {
-                    accountExists = true;
-                }
-            }
-
-            if (accountExists) {
-                output = "Sorry, the user already exists";
-            } else {
-                MessagingServer.authTokensCounter++;
-                Account account = new Account(username, MessagingServer.authTokensCounter);
-                MessagingServer.accounts.add(account);
-                output = String.valueOf(account.authToken);
-            }
-        }
-        return output;
-    }
-
-    private boolean IsUsernameValid(String username)
-    {
-        if(username.replaceAll("[a-zA-Z0-9_]", "").isBlank())
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    public String ShowAccounts() throws RemoteException
-    {
-        String output="";
-        int i;
-        for(i=0;i<MessagingServer.accounts.size()-1;i++)
-        {
-            output=output+(i+1)+". "+MessagingServer.accounts.get(i).username+"\n";
-        }
-        output=output+(i+1)+". "+MessagingServer.accounts.get(i).username;
-        return output;
-    }
-
-    public String SendMessage(int senderAuthToken, String recipientUsername, String messageBody) throws RemoteException
-    {
-        boolean recipientFound=false;
-        Account recipient=null;
-        for(Account acc:MessagingServer.accounts)
-        {
-            if(acc.username.equals(recipientUsername))
-            {
-                recipient=acc;
-                recipientFound=true;
-            }
-        }
-
-        String output;
-        if(recipientFound==true)
-        {
-            String senderUsername = null;
-            for(Account acc:MessagingServer.accounts)
-            {
-                if(acc.authToken==senderAuthToken)
-                {
-                    senderUsername=acc.username;
-                }
-            }
-
-
-            MessagingServer.messageIdsCounter++;
-            Message message=new Message(senderUsername, recipientUsername, messageBody, MessagingServer.messageIdsCounter);
-            recipient.messageBox.add(message);
-            output="OK";
-        }
-        else
-        {
-            output="User does not exist";
-        }
-
-        return output;
-
-    }
-
-    public String ShowInbox(int authToken) throws RemoteException
-    {
-        String output="";
-
-        Account account=null;
-        for(Account acc:MessagingServer.accounts)
-        {
-            if(acc.authToken==authToken)
-            {
-                account=acc;
-            }
-        }
-        for (Message message:account.messageBox)
-        {
-            output=output+message.info()+"\n";
-        }
-
-        return output;
     }
 
     public boolean CheckAuthToken(int authToken) throws RemoteException
@@ -141,6 +28,128 @@ public class RemoteMessagingApp extends UnicastRemoteObject implements Messaging
         {
             return false;
         }
+    }
+
+    private boolean IsUsernameValid(String username)
+    {
+        if(username.replaceAll("[a-zA-Z0-9_]", "").isEmpty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public String CreateAccount(String username) throws RemoteException
+    {
+        String output;
+        if(!IsUsernameValid(username))
+        {
+            output="Invalid Username";
+        }
+        else
+        {
+            boolean accountExists = false;
+            for (Account account : MessagingServer.accounts)
+            {
+                if (account.username.equals(username))
+                {
+                    accountExists = true;
+                }
+            }
+
+            if (accountExists)
+            {
+                output = "Sorry, the user already exists";
+            }
+            else
+            {
+                MessagingServer.authTokensCounter++;
+                Account account = new Account(username, MessagingServer.authTokensCounter);
+                MessagingServer.accounts.add(account);
+                output = String.valueOf(account.authToken);
+            }
+        }
+        return output;
+    }
+
+
+    public String ShowAccounts() throws RemoteException
+    {
+        String output="";
+        int i;
+        for(i=0;i<=MessagingServer.accounts.size()-1;i++)
+        {
+            output=output+(i+1)+". "+MessagingServer.accounts.get(i).username+"\n";
+        }
+        output=output.substring(0, output.length()-1);
+        return output;
+    }
+
+    public String SendMessage(int senderAuthToken, String recipientUsername, String messageBody) throws RemoteException
+    {
+        boolean recipientFound=false;
+        Account recipient=null;
+        for(Account acc:MessagingServer.accounts)
+        {
+            if(acc.username.equals(recipientUsername))
+            {
+                recipient=acc;
+                recipientFound=true;
+            }
+        }
+
+        String output;
+        if(recipientFound==false)
+        {
+            output="User does not exist";
+        }
+        else
+        {
+            String senderUsername = null;
+            for(Account acc:MessagingServer.accounts)
+            {
+                if(acc.authToken==senderAuthToken)
+                {
+                    senderUsername=acc.username;
+                }
+            }
+
+
+            MessagingServer.messageIdsCounter++;
+            Message message=new Message(senderUsername, recipientUsername, messageBody, MessagingServer.messageIdsCounter);
+            recipient.messageBox.add(message);
+            output="OK";
+
+
+        }
+
+
+        return output;
+
+    }
+
+    public String ShowInbox(int authToken) throws RemoteException
+    {
+        String output="";
+
+        Account account=null;
+        for(Account acc:MessagingServer.accounts)
+        {
+            if(acc.authToken==authToken)
+            {
+                account=acc;
+            }
+        }
+        for (Message message:account.messageBox)
+        {
+            output=output+message.info()+"\n";
+        }
+        output=output.substring(0, output.length()-1);
+
+        return output;
     }
 
     public String ReadMessage(int receiverAuthToken, int messageId) throws RemoteException
